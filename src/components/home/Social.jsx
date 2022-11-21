f354a915ee5059b022a37dbba0c79bb18c195086
Dayana Lony <dayanalonyama2001@gmail.com>
2022-11-21 18:04:54 +0100

    initialisation du projet

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a
+        href="https://www.instagram.com/diana_lonyama/"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-instagram"></i>
+      </a>
+
+      <a href="" className="home__social-icon" target="_blank">
+        <i class="uil uil-dribbble"></i>
+      </a>
+
+      <a
+        href="https://github.com/DAYANALONYAMA"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-github-alt"></i>
+      </a>
+    </div>
+  );
+};
+export default Social;
