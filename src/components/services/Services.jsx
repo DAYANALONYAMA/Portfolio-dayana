5f3e3ebb405969f0bcd8dabd41803bdb96484ea7
Dayana Lony <dayanalonyama2001@gmail.com>
2022-11-28 15:50:49 +0100

    feat:service

---
+++ src/components/services/Services.jsx
@@ -0,0 +1,113 @@
+import React, { useState } from "react";
+import "./services.css";
+
+const Services = () => {
+  const [toggleState, setToggleState] = useState(0);
+  const toggleTab = (index) => {
+    setToggleState(index);
+  };
+  return (
+    <section className="services section" id="services">
+      <h2 className="section__title">Services</h2>
+      <span className="section__subtitle">What i offer</span>
+      <div className="services__container container grid">
+        <div className="services__content">
+          <div>
+            <i className="uil uil-web-grid services__icon"></i>
+            <h3 className="services__title">
+              Product <br /> Designer
+            </h3>
+          </div>
+          <span className="services__button" onClick={() => toggleTab(1)}>
+            View More
+            <i className="uil uil-arrow-right services__button-icon"></i>
+          </span>
+          <div
+            className={
+              toggleState === 1
+                ? "services__modal  active-modal"
+                : "services__modal"
+            }
+          >
+            <div className="services__modal-content">
+              <i
+                onClick={() => toggleTab(0)}
+                className="uil uil-times sevices__modal-close"
+              ></i>
+              <h3 className="services__modal-title"></h3>
+              <p className="services__modal-description">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia
+                illo iste perspiciatis, sequi odit a voluptatem veritatis eum
+                quas nobis dolores porro voluptatibus! Perferendis hic placeat
+                nostrum tenetur nam nulla!
+              </p>
+              <ul className="services__modal-services grid">
+                <li className="services__modal-service">
+                  <i className="uil uil-check-circle services__modal-icon"></i>
+                  <p className="services__modal-info">
+                    I develop the user interface
+                  </p>
+                </li>
+                <li className="services__modal-service">
+                  <i className="uil uil-check-circle services__modal-icon"></i>
+                  <p className="services__modal-info">Web page development</p>
+                </li>
+                <li className="services__modal-service">
+                  <i className="uil uil-check-circle services__modal-icon"></i>
+                  <p className="services__modal-info">
+                    I develop the user interface
+                  </p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div className="services__content">
+          <div>
+            <i className="uil uil-arrow services__icon"></i>
+            <h3 className="services__title">
+              Ui/Ux <br /> Designer
+            </h3>
+          </div>
+          <pan className="services__button">
+            View More
+            <i className="uil uil-arrow-right services__button-icon"></i>
+          </pan>
+          <div className="services__modal">
+            <div className="services__modal-content">
+              <i className="uil uil-times sevices__modal-close"></i>
+              <h3 className="services__modal-title"></h3>
+              <p className="services__modal-description">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia
+                illo iste perspiciatis, sequi odit a voluptatem veritatis eum
+                quas nobis dolores porro voluptatibus! Perferendis hic placeat
+                nostrum tenetur nam nulla!
+              </p>
+              <ul className="services__modal-services grid">
+                <li className="services__modal-service">
+                  <i className="uil uil-check-circle services__modal-icon"></i>
+                  <p className="services__modal-info">
+                    I develop the user interface
+                  </p>
+                </li>
+                <li className="services__modal-service">
+                  <i className="uil uil-check-circle services__modal-icon"></i>
+                  <p className="services__modal-info">Web page development</p>
+                </li>
+                <li className="services__modal-service">
+                  <i className="uil uil-check-circle services__modal-icon"></i>
+                  <p className="services__modal-info">
+                    I develop the user interface
+                  </p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Services;
